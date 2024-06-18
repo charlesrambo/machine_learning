e783dfed5aa9443fb3c6fe8864207d92dcb9fa79
Charles Rambo <76271967+charlesrambo@users.noreply.github.com>
2024-06-18 14:14:07 -0700

    Added more functions
    Mostly chapter 12 of Advances in Financial Machine Learning.

--- lopez_de_prado.py
+++ lopez_de_prado.py
@@ -13,6 +13,8 @@ from sklearn.metrics import log_loss
 from sklearn.model_selection._split import KFold
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import BaggingClassifier, BaggingRegressor
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.pipeline import Pipeline
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples
 from sklearn.utils import check_random_state
@@ -20,6 +22,10 @@ from scipy.linalg import block_diag
 from sklearn.metrics import mutual_info_score
 import scipy.stats as stats
 import multiprocessing as mp
+from scipy.special import comb
+
+from sklearn.model_selection._split import  _BaseKFold
+from itertools import combinations
 
 
 # === Content from Machine Learning for Asset Managers ===
@@ -86,16 +92,18 @@ def featImpMDI(clf, feat_names):
     
     return imp
 
-def featImpMDA(clf, X, y, n_splits = 10):
+def featImpMDA(clf, X, y, n_splits = 10, cv_gen = None):
     
-    # Initialize k-folds constructor
-    cv_gen = KFold(n_splits = n_splits)
+    if cv_gen is None:
+        
+        # Initialize k-folds constructor
+        cv_gen = KFold(n_splits = n_splits).split(X = X)
     
     # Initialize pandas objects to hold raw and shuffled log_loss scores
     score_raw, score_shuff = pd.Series(), pd.DataFrame(columns = X.columns)
     
     # Generate split
-    for fold, (train_idx, test_idx) in enumerate(cv_gen.split(X = X)):
+    for fold, (train_idx, test_idx) in enumerate(cv_gen):
         
         # Create training arrays
         X_train, y_train = X.iloc[train_idx, :], y.iloc[train_idx]
@@ -139,19 +147,22 @@ def featImpMDA(clf, X, y, n_splits = 10):
     
     return imp
 
-def regFeatImpMDA(reg, X, y, n_splits = 10, p = 2):
+
+def regFeatImpMDA(reg, X, y, n_splits = 10, p = 2, cv_gen = None):
     
     # Define penalty function
     pen_fun = lambda e: np.sum(np.abs(e)**p)
     
-    # Initialize k-folds constructor
-    cv_gen = KFold(n_splits = n_splits)
-    
+    if cv_gen is None:
+        
+        # Initialize k-folds constructor
+        cv_gen = KFold(n_splits = n_splits).split(X = X)
+        
     # Initialize pandas objects to hold raw and shuffled log_loss scores
     score_raw, score_shuff = pd.Series(), pd.DataFrame(columns = X.columns)
     
     # Generate split
-    for fold, (train_idx, test_idx) in enumerate(cv_gen.split(X = X)):
+    for fold, (train_idx, test_idx) in enumerate(cv_gen):
         
         # Create training arrays
         X_train, y_train = X.iloc[train_idx, :], y.iloc[train_idx]
@@ -942,4 +953,253 @@ def run_queued_multiprocessing(func, index, params_dict = {},
     # Sort by the index
     result_df = result_df.sort_index()   
     
-    return result_df      
+    return result_df   
+
+    
+class CombPurgedKFoldCV(_BaseKFold):
+    """
+    Lopez de Prado's combinatorially purged k-folds cross-validation. This 
+    implementation performs k-fold cross-validation while purging data points 
+    based on predefined holding periods, specified in the holding_dates data
+    frame, as well as purge and embargo periods.
+  
+    Parameters:
+        
+        n_splits : int
+            Number of folds for k-fold CV.
+        
+        n_test_splits : int
+            Number of test splits per fold (must be between 1 and n_splits-1).
+        
+        holding_dates : pd.Series or pd.DataFrame
+            Pandas object with timestamps representing holding periods.
+        
+        purge : pd.Timedelta
+            Time delta for purging data before the holding period.
+        
+        embargo : pd.Timedelta
+            Time delta for purging data after the holding period.
+        
+        warm_up_end : pd.Timestamp, optional
+            End date for the warm-up period (if any).
+        
+        fixed_width : pd.Timedelta, optional
+            Fixed width for the holding periods if dates not provided in 
+            holding_dates.
+    """
+    
+    def __init__(self, n_splits = 5, n_test_splits = 2, holding_dates = None, 
+                 purge = pd.Timedelta(days = 0), embargo = pd.Timedelta(days = 0), 
+                 warm_up_end = None, fixed_width = None, safe = False):
+        
+        if not isinstance(holding_dates, pd.Series)|isinstance(holding_dates, pd.DataFrame):
+            
+            raise ValueError('Holding dates must be a pandas series or data frame.')
+            
+        elif isinstance(holding_dates, pd.Series):
+            
+            holding_dates = pd.DataFrame(holding_dates)
+        
+        # Save holding_dates as a class object
+        self.holding_dates = holding_dates.copy()
+        
+        if n_test_splits <= 0 or n_test_splits >= n_splits - 1:
+            
+            raise ValueError(f'K-fold cross-validation requires at least one train/test split.'
+                             f'This requires n_test_splits to be between 1 and n_splits - 1, inclusive. '
+                             f'Got n_test_splits = {n_test_splits}.')
+        
+        if fixed_width is not None:
+            
+            self.holding_dates['t1'] = self.holding_dates['t0'] + fixed_width
+            
+        else:
+            
+            if 't1' not in self.holding_dates:
+                
+                raise ValueError("The pandas object holding_dates must include a column 't1' or you must specify fixed_width.")
+                     
+        # Save splits as a class object
+        self.n_splits = int(n_splits)
+        
+        # Save test splits as a class object
+        self.n_test_splits = int(n_test_splits)
+        
+        # Save the purge time delta as a class object
+        self.purge = purge
+        
+        # Save the embargo time delta as a class object
+        self.embargo = embargo
+        
+        # Save the warm_up_end as a class object
+        self.warm_up_end = pd.Timestamp(warm_up_end)
+        
+        # Calculate number of paths
+        self.path_count = (n_test_splits/n_splits) * comb(n_splits, n_test_splits)
+        
+        # Save save for get_n_splits
+        self.safe = safe
+
+
+    # Create method to clean up train
+    def prep_train(self, train_splits, test_splits):
+            
+        # Start time of test set minus purge
+        start_times = [np.min(a) - self.purge for a in test_splits] 
+        
+        # End time of test plus embargo; add one hour to fix endpoint problem
+        end_times = [np.max(a) + self.embargo + pd.Timedelta(hours = 1) for a in test_splits]
+        
+        # Initialize is_bad
+        is_bad = pd.Series(False, index = self.holding_dates.index)
+        
+        # Remove outer nesting
+        train_dates = [date for a in train_splits for date in a]
+        
+        for i in range(len(test_splits)):
+             
+            # Train envelopes test
+            envelopes = (self.holding_dates['t0'] <= start_times[i]) & (
+                    self.holding_dates['t1'] >= end_times[i])
+            
+            # Starts in
+            starts_in = self.holding_dates['t0'].between(
+                start_times[i], end_times[i], inclusive = 'left')
+            
+            # Ends in
+            ends_in = self.holding_dates['t1'].between(
+                start_times[i], end_times[i], inclusive = 'right')
+                     
+            # Three cases:
+            
+            # (1) the train envelopes test
+            is_bad = is_bad|envelopes 
+            
+            # (2) train starts inside test 
+            is_bad = is_bad|starts_in
+            
+            # (3) train ends inside test
+            is_bad = is_bad|ends_in
+            
+        # What do we want to keep?
+        to_keep = self.holding_dates['t0'].isin(train_dates) & ~is_bad
+         
+        # Train index values
+        train_idx = list(self.holding_dates.loc[to_keep, :].index)
+        
+        return train_idx
+    
+    
+    # Create method to clean up test   
+    def prep_test(self, test_splits):
+        
+        # Convert test_splits to index values; prep_train already did it for train_splits
+        test_idx = np.concatenate([self.holding_dates.loc[self.holding_dates['t0'].isin(a), 
+                                                      't0'].index for a in test_splits])
+        
+        return test_idx.tolist()
+        
+        
+    def split(self, X, y = None, groups = None):
+        
+        # Check if index lines up
+        if np.any(X.index != self.holding_dates.index):
+            
+            raise ValueError('X and holding dates must have the same index')
+          
+        # Is it a warm up date?
+        is_warm_up = self.holding_dates['t0'] <= self.warm_up_end
+        
+        # Get the non-warm up dates
+        splits = np.array_split(self.holding_dates.loc[~is_warm_up, 't0'].unique(), 
+                                self.n_splits) 
+        
+        # Save missing stuff 
+        warm_up_dates = list(self.holding_dates.loc[is_warm_up, 't0'].unique())
+            
+        # Convert to list so no trouble
+        splits = [list(a) for a in splits]
+        
+        # Loop over splits of index
+        for test_splits in combinations(splits, r = self.n_test_splits):
+            
+            # Get train date lists; add back warm up dates which may be empty
+            train_splits = [warm_up_dates] + [a for a in splits if a not in test_splits]
+            
+            # Fix train; remove layer of brackets, bad observations, convert to index values
+            train_idx = self.prep_train(train_splits, test_splits)
+            
+            # Remove layer of brackets and convert to index values
+            test_idx = self.prep_test(test_splits)
+               
+            # Not helpful if either list is empty
+            if len(train_idx) == 0 or len(test_idx) == 0:
+                
+                continue
+            
+            else:
+                
+                yield train_idx, test_idx
+                
+
+    def get_n_splits(self, X, y = None, groups = None):
+        # Method called when fitting values, and throws an error since length of cv isn't n_splits
+
+        # If safe is true, simply count the number of splits by generating them
+        if self.safe:
+            
+            return len(list(self.split(X)))
+        
+        # IF safe is false is combintation; pay over estimate because of purge and embargo
+        else:
+            
+            return int(comb(self.n_splits, self.n_test_splits))
+                
+                
+# Lopez de Prado's fix for annoying Pipeline syntax 
+# https://stackoverflow.com/questions/36205850/sklearn-pipeline-applying-sample-weights-after-applying-a-polynomial-feature-t
+class MyPipeline(Pipeline):
+    
+    def fit(self, X, y, sample_weights = None, **fit_params):
+        
+        if sample_weights is not None:
+            
+            fit_params[self.steps[-1][0] + '__sample_weight'] = sample_weights
+            
+        return super().fit(X, y, **fit_params)
+
+                
+def clf_hyperparameter_fit(X, y, holding_dates, pipe_clf, param_grid, scoring, 
+                           inner_cv, bagging_dict = None, n_random_iter = 0, 
+                           n_jobs = -1, **fit_params):
+     
+    # If n_random_iter is 0...
+    if n_random_iter == 0:
+        
+        # ... just perform regular grid search
+        gs = GridSearchCV(estimator = pipe_clf, param_grid = param_grid,
+                          scoring = scoring, cv = inner_cv, n_jobs = n_jobs)
+    
+    # Otherwise...
+    else:
+        
+        # ... use random search
+        gs = RandomizedSearchCV(estimator = pipe_clf, 
+                                param_distributions = param_grid,
+                                scoring = scoring, cv = inner_cv, 
+                                n_jobs = n_jobs, n_iter = n_random_iter)
+     
+    # Fit grid search and record the best estimator
+    gs = gs.fit(X, y, **fit_params).best_estimator_ 
+        
+    # fit validated model on the entirety of the data
+    if bagging_dict is not None:
+        
+        gs = BaggingClassifier(estimator = MyPipeline(gs.steps), n_jobs = n_jobs, 
+                               **bagging_dict)
+        
+        gs = gs.fit(X, y, sample_weight = fit_params[gs.base_estimator.steps[-1][0] + '__stample_weight'])
+        
+        gs = Pipeline([('bag', 'gs')])
+        
+    return gs
